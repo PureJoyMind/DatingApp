d1e6afe6a2604acd098255215dd443f6fe9ff471
PureJoyMind <mazdak.parniandg@gmail.com>
2024-09-05 18:34:25 +0330

    basic package installation and checkig if basic projects can connect

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2c283f402759813fef3d1f79c73095c90fc37d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c1e5b9b0a068b058fe1d42823e06fc378bd5dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -14,10 +15,10 @@ export class AppComponent implements OnInit {
   title = 'DatingApp';
   users: any;
   ngOnInit(): void {
-    const users = this.http.get("https://localhost/api/users").subscribe({
+    const users = this.http.get("https://localhost:5001/api/users").subscribe({
       next: res => this.users = res,
       error: error => console.log(error),
-      complete: () => console.log("Request complete"),      
+      complete: () => console.log("Request complete", users),      
     });
     
   }
