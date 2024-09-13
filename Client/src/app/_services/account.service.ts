ee9458a169ed1c9c4c3541da956f6815934d1621
PureJoyMind <mazdak.parniandg@gmail.com>
2024-09-13 15:11:53 +0330

    Added Types, styled the user profile and matches page, added image gallery

--- Client/src/app/_services/account.service.ts
+++ Client/src/app/_services/account.service.ts
@@ -3,13 +3,14 @@ import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../models/user';
 import { map } from 'rxjs';
 import { JsonPipe } from '@angular/common';
+import { environment } from '../../environments/environment';
  
 @Injectable({ // These are Singletons
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null)
   login(model : any){
     return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
